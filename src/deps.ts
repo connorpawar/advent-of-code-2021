93a92bef570080e960a264e3c489bb991bc0201f
unknown <connorpawar@gmail.com>
2021-11-28 19:36:04 -0600

    Add typed_ds_algs deno package

---
+++ src/deps.ts
@@ -0,0 +1,2 @@
+import { Stack, Queue, BinarySearchTree, BinaryTreeNode } from "https://deno.land/x/typed_ds_algs@v0.3.0/deno_lib/index.ts";
+export { Stack, Queue, BinaryTreeNode, BinarySearchTree };
